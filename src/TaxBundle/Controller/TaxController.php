abae263d357ec6d1567fa1432a2e47640e8c634d
Ovidiu Maghetiu <ovidiumght@gmail.com>
2016-03-13 13:46:08 +0200

    Basic exception handling

--- src/TaxBundle/Controller/TaxController.php
+++ src/TaxBundle/Controller/TaxController.php
@@ -4,21 +4,31 @@ namespace TaxBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Tax\Exception\InvalidTaxRateException;
 use Tax\TaxService;
 
 class TaxController extends Controller
 {
     public function indexAction()
     {
-        /** @var TaxService $taxService */
-        $taxService = $this->get('tax.tax_service');
+        try{
+            /** @var TaxService $taxService */
+            $taxService = $this->get('tax.tax_service');
 
-        return $this->render('TaxBundle:Tax:tax.html.php',[
-            'overAllTaxCollectedPerState' => $taxService->calculateOverallTaxCollectedPerState(),
-            'averageTaxCollectedPerState' => $taxService->calculateAverageTaxCollectedPerState(),
-            'averageCountyTaxRatePerState' => $taxService->calculateAverageCountyTaxRatePerState(),
-            'averageCountryTaxRate' => $taxService->calculateAverageTaxRate(),
-            'allTaxesCollected' => $taxService->calculateAllTaxesCollected()
-        ]);
+            $taxData = [
+                'overAllTaxCollectedPerState' => $taxService->calculateOverallTaxCollectedPerState(),
+                'averageTaxCollectedPerState' => $taxService->calculateAverageTaxCollectedPerState(),
+                'averageCountyTaxRatePerState' => $taxService->calculateAverageCountyTaxRatePerState(),
+                'averageCountryTaxRate' => $taxService->calculateAverageTaxRate(),
+                'allTaxesCollected' => $taxService->calculateAllTaxesCollected()
+            ];
+        } catch (InvalidTaxRateException $e) {
+            return new Response('The data provided is invalid');
+        } catch (\Exception $e) {
+            return new Response('An unknown error occured');
+        }
+
+
+        return $this->render('TaxBundle:Tax:tax.html.php', $taxData);
     }
 }
